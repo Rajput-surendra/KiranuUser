c222ad86db56f174ce97384622cc68e208c5b2ea
Rajput-surendra <surendraalphawizz@gmail.com>
2023-10-31 20:48:46 +0530

    Initial commit

---
+++ KiranuUser/lib/Helper/Constant.dart
@@ -0,0 +1,19 @@
+final String appName = 'Burans';
+
+final String packageName = 'com.kiranu.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.kiranu.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'com.kiranu.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+final String baseUrl = 'https://developmentalphawizz.com/kiranu/app/v1/api/';
+final String imageUrl = 'https://developmentalphawizz.com/burans/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
